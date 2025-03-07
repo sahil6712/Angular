230c15e9afadaf9f47caa564f1c46a07683cbbbb
Sahil <sahilkhemariya327@gmail.com>
2025-03-07 12:30:13 +0530

    [Sahil] Add. Validation to name added

--- HelloWorld/src/app/app.component.ts
+++ HelloWorld/src/app/app.component.ts
@@ -16,7 +16,8 @@ export class AppComponent {
   imgUrl = "https://i.pinimg.com/736x/71/d9/a9/71d9a9a5614ec8a9a17079ace15f10a0.jpg";
 
   userName: string = "";
-
+  nameError: string = "";
+  
   ngOnInit(): void {
     this.title = "Hello from brigdeLabz!";
   }
@@ -24,5 +25,15 @@ export class AppComponent {
   onClick($event: any) {
     console.log("Save button is clicked!", $event);
     window.open(this.url, "_blank");
+  }
+
+  onInput($event:any) {
+    console.log("Change event occured!", $event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if (nameRegex.test(this.userName)) {
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is incorrect";
   }
 }
